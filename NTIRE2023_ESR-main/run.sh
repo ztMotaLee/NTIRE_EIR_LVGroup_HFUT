fa3d3bf461b8e2515250622060b7fb4752323877
MotaLee <92512536+ztMotaLee@users.noreply.github.com>
2023-03-21 02:56:50 +0800

    Add files via upload

---
+++ NTIRE2023_ESR-main/run.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir /root/autodl-tmp/datasets --save_dir  /root/autodl-tmp/NTIRE2023_Challenge/results --model_id 0
+
+CUDA_VISIBLE_DEVICES=0 python test_demo.py --data_dir /root/autodl-tmp/datasets --save_dir  /root/autodl-tmp/NTIRE2023_Challenge/results --model_id 1
